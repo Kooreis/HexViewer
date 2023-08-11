fc7845d94bb1fd3caf0597db4066439f9be5f8be
Kooreis <kooreis.official@gmail.com>
2023-08-11 02:23:47 -0500

    Imported necessary classes for file input/output and handling exceptions.

--- Hex_Viewer.java
+++ Hex_Viewer.java
@@ -1,26 +1,2 @@
-```java
 import java.io.FileInputStream;
 import java.io.IOException;
-
-public class HexViewer {
-    public static void main(String[] args) {
-        if (args.length < 1) {
-            System.out.println("Usage: java HexViewer <file>");
-            return;
-        }
-
-        try (FileInputStream fis = new FileInputStream(args[0])) {
-            int value;
-            int count = 0;
-            while ((value = fis.read()) != -1) {
-                System.out.printf("%02X ", value);
-                if (++count % 16 == 0) {
-                    System.out.println();
-                }
-            }
-        } catch (IOException e) {
-            System.out.println("Error reading file: " + e.getMessage());
-        }
-    }
-}
-```
