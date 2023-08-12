2b9d281e566d5970a30cdc026b67e7d390bd577e
Kooreis <kooreis.official@gmail.com>
2023-08-12 02:23:47 -0500

    Defined the main class and main method. Added a check to ensure that a file name is provided as an argument.

--- Hex_Viewer.java
+++ Hex_Viewer.java
@@ -1,2 +1,6 @@
-import java.io.FileInputStream;
-import java.io.IOException;
+public class HexViewer {
+    public static void main(String[] args) {
+        if (args.length < 1) {
+            System.out.println("Usage: java HexViewer <file>");
+            return;
+        }
